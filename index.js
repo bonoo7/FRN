67c0ef97437620fc0c0998a0e6f4373c1d6c1dc6
M7MD .. <6rga3ah511@gmail.com>
2025-02-12 18:37:09 +0300

    Fix web configuration

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+import { AppRegistry, Platform } from 'react-native';
 import { registerRootComponent } from 'expo';
 import App from './App';
 
-registerRootComponent(App);
+if (Platform.OS === 'web') {
+  AppRegistry.registerComponent('main', () => App);
+  AppRegistry.runApplication('main', {
+    initialProps: {},
+    rootTag: document.getElementById('root')
+  });
+} else {
+  registerRootComponent(App);
+}
